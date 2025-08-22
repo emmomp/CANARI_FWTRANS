9e44df19f89666a7ad535e039cde32993b0d5c65
Emma Boland <emmomp@bas.ac.uk>
2025-08-22 12:50:19 +0100

    added extra exf labels and directories, switched ecco_grid file

--- code/inputs.py
+++ code/inputs.py
@@ -14,16 +14,19 @@ import xarray as xr
 
 FCNAME = "horflux_fw_denm"
 CONV_DIR = "../data_out/denm_X_ECCOclimanom_hfreq"
+PERTCONV_DIR = "../data_out/denm_X_perts_hfreq"
 EV_DIR = "../data_out/ev"
 RECON_DIR = "../data_out/reconstructions"
 EXPDIR = "/users/emmomp/data/canari/experiments"
 GRIDDIR = "/users/emmomp/data/orchestra/grid2/"
+SOLN_DIR='/data/expose/ECCOv4-r4/Version4/Release4/nctiles_monthly'
 CONTR_DIR = "../data_out/contrs"
+DATA_DIR= "../data_out"
 
 TRANSP = "fw"
 FCNAME = "horflux_fw_denm"
 
-ecco_grid = xr.open_dataset("~/data/orchestra/other_data/ECCO_r3_alt/ECCOv4r3_grid.nc")
+ecco_grid=xr.open_dataset('/data/expose/ECCOv4-r4/Version4/Release4/nctiles_grid/ECCO-GRID.nc') 
 
 eyears = ["2006", "2014", "2000"]
 adj_diag_map = {
@@ -36,7 +39,12 @@ adj_diag_map = {
 adj_units=dict(zip(['adxx_qnet','adxx_empmr','adxx_tauu','adxx_tauv'],['W/m$^2$','m/s','m$^2$/s','m$^2$/s']))
 adj_labels=dict(zip(['adxx_qnet','adxx_empmr','adxx_tauu','adxx_tauv'],['Net Heat Flux','Net Freshwater Flux','Zonal Wind Stress','Meridional Wind Stress']))
 
-masks_labels=dict(zip(['global','egland','natl','arct','gin'],['Global','E Gland','N Atl','Arctic','GIN']))
+exf_vars=["oceQnet","EXFqnet" ,"oceTAUU", "EXFtauu","oceTAUV", "EXFtauv", "oceFWflx","EXFempmr","wind_OCE"]
+oce_vars=["oceQnet","oceTAUU", "oceTAUV", "oceFWflx","wind_OCE"]
+exf_units=dict(zip(exf_vars,['W/m$^2$','W/m$^2$','m$^2$/s','m$^2$/s','m$^2$/s','m$^2$/s','m/s','m/s','m$^2$/s']))
+exf_labels=dict(zip(exf_vars,['Net Heat Flux','Net Heat Flux','Zonal Wind Stress','Zonal Wind Stress','Meridional Wind Stress','Meridional Wind Stress','Net Freshwater Flux','Net Freshwater Flux','Wind Stress']))
+
+masks_labels=dict(zip(['global','egland','natl','arct','gin','hudson', 'north', 'baffin','barents','gland','norw'],['Global','E Gland','N Atlantic','Arctic','GIN','Hudson','North','Baffin','Barents','Greenland','Norwegian']))
 
 ecco_convs = {}
 ecco_convs["all"] = []
