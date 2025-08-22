8f69ddd1d3b8046a0b76050d4a2659423cb89f26
Emma Boland <emmomp@bas.ac.uk>
2025-08-22 12:55:28 +0100

    adding denmark_strait_*vars.py to extract model variables at DS

---
+++ code/denmark_strait_ctrlvars.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+denmark_strait_ctrlvars.py
+
+Extract variables from Denmark Strait in the ECCOv4r4 solution
+
+Required to reproduce data for Boland et al. 2025 (in prep)
+See https://github.com/emmomp/CANARI_FWTRANS for details
+
+Updated Jun 2025
+
+@author: emmomp@bas.ac.uk Emma J D Boland
+"""
+from datetime import date
+import sys
+import xarray as xr
+
+sys.path.insert(0, "/users/emmomp/Python/ECCOv4-py")
+import ecco_v4_py as ecco
+from inputs import SOLN_DIR, ecco_grid
+
+SECTION = "Denmark Strait"
+
+[section_pt1, section_pt2] = ecco.get_section_endpoints(SECTION)
+line_maskC, line_maskW, line_maskS = ecco.get_section_line_masks(    
+    section_pt1, section_pt2, ecco_grid
+)
+
+ds_ctrl=[]
+for var in ['MXLDEPTH','SIarea','SALT','THETA','SSH']:
+    ds_ctrl.append(ecco.recursive_load_ecco_var_from_years_nc(SOLN_DIR, \
+                                               vars_to_load=[var,    ],years_to_load='all')    )  
+ds_ctrl=xr.merge(ds_ctrl+[ecco_grid,])
+
+ctrl_section=ds_ctrl.chunk({'time':121,'k':50,'tile':1,'j':20,'i':20}).where(line_maskC,drop=True).squeeze().stack({'ji':['j','i']}).dropna('ji').sortby('XC')
+ctrl_section.time.encoding.clear()
+ctrl_section.reset_index('ji').to_netcdf(f'../data_out/{SECTION.replace(" ","_")}_vars.nc')
+
+# Define halocline and find base
+base_mask=(ctrl_section['SALT']<=34.8)&((-ctrl_section['Z'])<ctrl_section['Depth'])
+base_depth=ecco_grid.Z[base_mask.argmin('k').load()-1]
+base_mask=xr.where(ecco_grid.Z>depth,1,0)
+base_mask.name='halocline_mask'
+base_mask=base_mask.assign_coords({'Z':ecco_grid['Z']})
+base_depth.name='halocline_depth'
+ds_out=xr.merge([base_mask,base_depth.drop_vars(['Z','PHrefC','drF'])])
+ds_out.reset_index('ji').to_netcdf(f'../data_out/{SECTION.replace(" ","_")}_halocline.nc')
---
+++ code/denmark_strait_pertvars.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+denmark_strait_ctrlvars.py
+
+Extract variables from Denmark Strait in perturbations to the ECCOv4r4 solution
+
+Required to reproduce data for Boland et al. 2025 (in prep)
+See https://github.com/emmomp/CANARI_FWTRANS for details
+
+Updated Jun 2025
+
+@author: emmomp@bas.ac.uk Emma J D Boland
+"""
+from datetime import date
+import sys
+import xarray as xr
+import numpy as np
+
+sys.path.insert(0, "/users/emmomp/Python/ECCOv4-py")
+import ecco_v4_py as ecco
+from inputs import SOLN_DIR, ecco_grid, EXPDIR, GRIDDIR
+
+SECTION = "Denmark Strait"
+pert='pert_10y_tauu_NGlandJANpulse'
+startdate=np.datetime64('1992-01-01T12:00:00')
+Sref=35
+
+[section_pt1, section_pt2] = ecco.get_section_endpoints(SECTION)
+line_maskC, line_maskW, line_maskS = ecco.get_section_line_masks(    
+    section_pt1, section_pt2, ecco_grid
+)
+
+extra_variables = dict( SSH = dict(dims=['j','i'], attrs=dict(standard_name='sea_surface_height_above_geoid', long_name='Dynamic sea surface height anomaly', units='m')))
+
+ds_plus=[]
+ds_minus=[]
+for var in ['MXLDEPTH','SIarea','SALT','THETA','SSH']:
+    ds_plus.append(ecco.load_ecco_vars_from_mds(f'{EXPDIR}/{pert}plus/diags/{var}_mon_mean',mds_grid_dir=GRIDDIR,mds_files=f'{var}_mon_mean',output_freq_code='AVG_MON',
+                                                    model_start_datetime=startdate,read_grid=False,extra_variables=extra_variables
+                                                   ))
+    ds_minus.append(ecco.load_ecco_vars_from_mds(f'{EXPDIR}/{pert}minus/diags/{var}_mon_mean',mds_grid_dir=GRIDDIR,mds_files=f'{var}_mon_mean',output_freq_code='AVG_MON',
+                                                    model_start_datetime=startdate,read_grid=False,extra_variables=extra_variables
+                                                    ))
+ds_plus=xr.merge(ds_plus).assign_coords({'type':'plus'})
+ds_minus=xr.merge(ds_minus).assign_coords({'type':'minus'})
+ds_perts=xr.concat([ds_plus,ds_minus],'type')
+ds_perts['FWC']=((1-ds_perts.SALT/Sref)*ecco_grid.drF*ecco_grid.hFacC*ecco_grid.rA).sum('k')
+ds_perts['FWC_z']=((1-ds_perts.SALT/Sref)*ecco_grid.drF*ecco_grid.hFacC*ecco_grid.rA)
+ds_section=ds_perts.chunk({'time':121,'k':50,'tile':1,'j':20,'i':20}).where(line_maskC,drop=True).squeeze().stack({'ji':['j','i']}).dropna('ji').sortby('XC')
+
+ds_section.reset_index('ji').to_netcdf(f'../data_out/{SECTION.replace(" ","_")}_{pert}.nc')
