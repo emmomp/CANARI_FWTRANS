ae89527e0801a275222a8d9c81dbfc6c16ae59b0
Emma Boland <emmomp@bas.ac.uk>
2026-01-28 10:54:55 +0000

    adding netcdf attributes to greenlandsea_mask

--- code/make_greenlandsea_mask.py
+++ code/make_greenlandsea_mask.py
@@ -12,12 +12,19 @@ Updated Jan 2026
 
 @author: emmomp@bas.ac.uk Emma J D Boland
 """
+from datetime import date
 import xarray as xr
 import numpy as np
 from cartopy.io.shapereader import Reader
 from shapely import Point
+from inputs import ecco_grid
 
-ecco_grid = xr.open_dataset("~/data/orchestra/other_data/ECCO_r3_alt/ECCOv4r3_grid.nc")
+attrs = {
+    "contact": "emmomp@bas.ac.uk",
+    "references": "ECCOv4r4 data from Boland et al (in prep)",
+    "date": "Created on " + date.today().strftime("%d/%m/%Y"),
+    "notes": "Data produced by analysis of the ECCOv4r4 state estimate, see ecco-group.org",
+}
 
 shp=Reader('../other_data/iho_greenlandsea/iho.shp')
 geoms=shp.geometries()
@@ -37,4 +44,5 @@ for tile in [2,6]:
             else:
                 gland_mask.loc[dict(tile=tile,i=i,j=j)]=False
 
+gland_mask.attrs.update(attrs)
 gland_mask.to_netcdf('../other_data/greenlandsea_mask.nc')              
